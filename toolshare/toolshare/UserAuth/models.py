a6d5e991e7c04715a36a2f04ebdd9c33ea36f001
Vishnu Tej <vishnutej9492@gmail.com>
2014-09-24 15:23:43 -0400

    deleted:    UserAuth/__init__.py 	deleted:    UserAuth/admin.py 	deleted:    UserAuth/models.py 	deleted:    UserAuth/tests.py 	deleted:    UserAuth/urls.py 	deleted:    UserAuth/views.py

--- toolshare/toolshare/UserAuth/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-
-class Tooler(models.Model):
-    user = models.OneToOneField(User)
-    zipcode = models.IntegerField(default=0)
--- toolshare/toolshare/UserAuth/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns,url,include 
-
-from UserAuth import views
-
-urlpatterns = patterns('' ,
-         url(r'^$', views.login, name='login')
-         )
