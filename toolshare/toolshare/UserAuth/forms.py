9bcc90788010f035a31d00ae6f4b68988739e04b
Priyank Kapadia <priyankkapadia89@gmail.com>
2014-10-02 21:52:13 -0400

    Register Form: 1. Initial RegisterFormClass for SignUp 2. Provided a link on the siginform for Signup/Register

--- toolshare/toolshare/UserAuth/forms.py
+++ toolshare/toolshare/UserAuth/forms.py
@@ -3,3 +3,10 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username/email',max_length=100)
     password = forms.CharField(widget=forms.PasswordInput,label='Password',max_length=16)
+
+class RegisterForm(forms.Form):
+    firstname = forms.CharField(label ='First Name',max_length=100)
+    lastname = forms.CharField(label ='Last Name',max_length = 100)
+    email = forms.CharField(label = 'Your Email',max_length = 100)
+    city = forms.CharField(label = 'City',max_length = 100)
+    zipcode = forms.IntergerField(label = 'Zipcode', max_length = 6)
