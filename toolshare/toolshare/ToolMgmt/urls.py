73f22fe37d0ad5fbe337bc9377c4e8b95f3228f5
Ziyi Bai <baiziyi@ziyidembp.wireless.rit.edu>
2014-10-05 15:18:04 -0400

    Add tool detail page

--- toolshare/toolshare/ToolMgmt/urls.py
+++ toolshare/toolshare/ToolMgmt/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from ToolMgmt import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<tool_id>\d+)/$', views.detail, name='detail'),
 )
--- toolshare/toolshare/ToolMgmt/views.py
+++ toolshare/toolshare/ToolMgmt/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from ToolMgmt.models import Tool
+from django.http import HttpResponse
 
 def index(request):
     all_tools = Tool.objects.all()
     return render(request, 'ToolMgmt/index.html', {'all_tools': all_tools})
+
+def detail(request,tool_id):
+     return HttpResponse("You are looking at tool %s." % tool_id)
