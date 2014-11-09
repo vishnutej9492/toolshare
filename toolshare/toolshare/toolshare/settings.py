080d0c546cebaa961eba0fe85ca8825bbf863591
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2014-11-09 14:11:13 -0300

    Add image field to Tool model (need to run south migration)

--- toolshare/toolshare/toolshare/settings.py
+++ toolshare/toolshare/toolshare/settings.py
@@ -94,3 +94,5 @@ TEMPLATE_LOADERS = (
             'django.template.loaders.filesystem.Loader',
             'django.template.loaders.app_directories.Loader',
                 )
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
