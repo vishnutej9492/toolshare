4bef4a6726dad6095d0779c217abc54629b51d5e
Priyank Kapadia <priyankkapadia89@gmail.com>
2014-11-15 17:20:29 -0500

    Added some models to the Sharing application

--- toolshare/toolshare/Sharing/models.py
+++ toolshare/toolshare/Sharing/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from UserAuth.models import UserProfile
 # Create your models here.
+
+class ShareZone(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=100)
+    description = models.CharField(verbose_name="Description", max_length=200)
+    users = models.ForeignKey(UserProfile, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+class Shed(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=100)
+    description = models.CharField(verbose_name="Description", max_length=200)
+    sharezone = models.ForeignKey(ShareZone)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
