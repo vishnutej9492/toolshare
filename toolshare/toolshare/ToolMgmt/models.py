153923161c724048ea23322b061a7c3deeeedc6a
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2014-12-10 20:41:12 -0300

    Add blackout date to Tool model

--- toolshare/toolshare/ToolMgmt/models.py
+++ toolshare/toolshare/ToolMgmt/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from UserAuth.models import UserProfile
 from Sharing.models import ShareZone, Shed, Sharing, Request
 from django.db.models import Q
-import datetime
+from datetime import date, timedelta, datetime
 from django.utils.timezone import utc
 from Sharing.models import ShareZone, Shed
 
@@ -31,6 +31,8 @@ class Tool(models.Model):
     identifier = models.CharField(verbose_name="Identifier", blank=True, null=True, max_length=200)
     shed = models.ForeignKey(Shed, related_name='tools', null = True, blank = True)
     in_shed = models.BooleanField(default = False)
+    blackout_start_date = models.DateTimeField()
+    blackout_end_date = models.DateTimeField()
 
     def __str__(self):
         return self.name
@@ -45,3 +47,14 @@ class Tool(models.Model):
                                       ((Q(start_date__lte=from_date)&Q(end_date__gte=from_date)) |
                                       (Q(start_date__lte=to_date)&Q(end_date__gte=to_date))))
         return (tools.count() == 0)
+
+    def date_range(self, start, end):
+        d1 = datetime.strptime(start, '%Y/%m/%d')
+        d2 = datetime.strptime(end, '%Y/%m/%d')
+        l = []
+        for i in range((d2-d1).days + 1):
+            l.append( str( (d1+ timedelta(days=i)).date().strftime('%Y/%m/%d') ))
+        return l
+
+    def blackout_dates(self):
+        return self.date_range('2014/12/08','2014/12/19')
